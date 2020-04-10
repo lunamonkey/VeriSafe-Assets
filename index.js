54c330f59f52e6db60ae1e3057f7a0784e18c6d4
lunamonkey <lunamonkey@gmail.com>
2020-04-10 12:58:24 +0100

    Refactor project

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
-async function main(){
-	console.log("VeriSafe Assets:main()");
-	process.core = process.core || {};
-	process.core.assets = process.core.assets || {};
-
-	// retrieve settings from files
-	console.log("VeriSafe Assets: About to set assets.");
-	process.assets = require("./settingsAssets.json");
-
-	// put the contents of tokens, directly into assets (no parent container)
-
-	console.log("VeriSafe Assets: About to set core-assets-tokens from assets-tokens.");
-	Object.assign(process.core.assets, process.assets.tokens);
-	// put the contents of coins, directly into assets (no parent container)
-
-	console.log("VeriSafe Assets: About to set core-assets-coins from assets-coins.");
-	Object.assign(process.core.assets, process.assets.coins);
-
+console.log("VeriSafe Assets: About to set assets.");
+let settingsAssets = require("settingsAssets.json");
+let allAssets;
+function reBuildAssets() {
 	var decache = require('decache');
-	console.log("VeriSafe Assets: About to set up main jobs");
-	setInterval(() => {
-		console.log("VeriSafe Assets: About to get assets and reassign.");
-		decache("./settingsAssets.json");
-		process.assets = require("./settingsAssets.json");
-		// put the contents of tokens, directly into assets (no parent container)
-		console.log("VeriSafe Assets: About to reassign core-assets-tokens from assets-tokens.");
-		Object.assign(process.core.assets, process.assets.tokens);
-		// put the contents of coins, directly into assets (no parent container)
-		console.log("VeriSafe Assets: About to reassign core-assets-coins from assets-coins.");
-		Object.assign(process.core.assets, process.assets.coins);
-	}, 5 * 60 * 1000);
+ 	allAssets = {};
+	console.log("VeriSafe Assets: About to get assets and reassign.");
+	decache("settingsAssets.json");
+	settingsAssets = require("settingsAssets.json");
+	// put the contents of tokens, directly into assets (no parent container)
+	console.log("VeriSafe Assets: About to reassign core-assets-tokens from assets-tokens.");
+	Object.assign(allAssets, settingsAssets.tokens);
+	// put the contents of coins, directly into assets (no parent container)
+	console.log("VeriSafe Assets: About to reassign core-assets-coins from assets-coins.");
+	Object.assign(allAssets, settingsAssets.coins);
 }
 
-(async () => {
-    try {
-		console.log("VeriSafe Assets:() before");
-        await main();
-		console.log("VeriSafe Assets:() done");
-    } catch (e) {
-        /*eslint no-console: ["error", {allow: ["error"]}]*/
-        console.error("VeriSafe Assets:Main: error:(e)", e);
-	}
-})();
+module.exports = async () => {
+	console.log("VeriSafe Assets: About to set allAssets-tokens from settingsAssets-tokens.");
+	Object.assign(allAssets, settingsAssets.tokens);
+
+	console.log("VeriSafe Assets: About to set allAssets-coins from settingsAssets-coins.");
+	Object.assign(allAssets, settingsAssets.coins);
+
+	console.log("VeriSafe Assets: About to set up reBuild for updating assets");
+	setInterval(reBuildAssets(), 5 * 60 * 1000);
+	console.log("VeriSafe Assets:() done");
+	
+	return{
+		assets : allAssets,
+		coins : settingsAssets,
+		tokens : settingsAssets.tokens
+	};
+};
+
+
