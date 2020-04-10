08bf4a8ca563d1375b7268c76fbf02827d699481
lunamonkey <lunamonkey@gmail.com>
2020-04-10 22:40:51 +0100

    Refactor Assets

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 console.log("VeriSafe Assets: About to set assets.");
-let settingsAssets = require("settingsAssets.json");
+let settingsAssets = require("./settingsAssets.json");
 let allAssets;
 function reBuildAssets() {
-	var decache = require('decache');
- 	allAssets = {};
+	var decache = require("decache");
+	allAssets = {};
 	console.log("VeriSafe Assets: About to get assets and reassign.");
 	decache("settingsAssets.json");
 	settingsAssets = require("settingsAssets.json");
 	// put the contents of tokens, directly into assets (no parent container)
-	console.log("VeriSafe Assets: About to reassign core-assets-tokens from assets-tokens.");
+	console.log("VeriSafe Assets: About to reassign allAssets from settingsAssets.tokens.");
 	Object.assign(allAssets, settingsAssets.tokens);
 	// put the contents of coins, directly into assets (no parent container)
-	console.log("VeriSafe Assets: About to reassign core-assets-coins from assets-coins.");
+	console.log("VeriSafe Assets: About to reassign allAssets from settingsAssets.coins.");
 	Object.assign(allAssets, settingsAssets.coins);
 }
 
 module.exports = async () => {
-	console.log("VeriSafe Assets: About to set allAssets-tokens from settingsAssets-tokens.");
+	console.log("VeriSafe Assets: About to set allAssets-tokens from settingsAssets.tokens.");
 	Object.assign(allAssets, settingsAssets.tokens);
 
-	console.log("VeriSafe Assets: About to set allAssets-coins from settingsAssets-coins.");
+	console.log("VeriSafe Assets: About to set allAssets-coins from settingsAssets.coins.");
 	Object.assign(allAssets, settingsAssets.coins);
 
 	console.log("VeriSafe Assets: About to set up reBuild for updating assets");
-	setInterval(reBuildAssets(), 5 * 60 * 1000);
+	setInterval(reBuildAssets(), 0.1 * 60 * 1000);
 	console.log("VeriSafe Assets:() done");
 	
 	return{
-		assets : allAssets,
-		coins : settingsAssets,
-		tokens : settingsAssets.tokens
+		allAssets : allAssets,
+		onlyCoins : settingsAssets.coins,
+		onlyTokens : settingsAssets.tokens
 	};
 };
-
-
